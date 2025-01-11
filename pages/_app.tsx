fda1219e7eba8e138341311b3814efe180dbf6cf
Somil Singhai <somilsinghai1804@gmail.com>
2025-01-11 15:57:03 +0530

    react-notion-x trial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,8 @@
 import "tailwindcss/tailwind.css"
+import "react-notion-x/src/styles.css"
+import "prismjs/themes/prism-tomorrow.css"
+import "katex/dist/katex.min.css"
+
 import { nunito } from "@/lib/fonts"
 
 function App({ Component, pageProps }) {
